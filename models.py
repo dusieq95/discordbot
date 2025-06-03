582bfbecae66ecd0fa5de5a9ea26f8dc2cdd3986
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2025-06-03 20:08:44 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,10 +6,14 @@ class DBChannel(Model):
   active = fields.BooleanField(default=lambda:False)
 
   def toggle_active(self):
-    return self.update(active=not self.active)
+    self.active=not self.active
+    return self.save()
 
 class DBUser(Model):
   id = fields.BigIntField(pk=True)
   sid = fields.CharField(unique=True, max_length=50)
   auth_token = fields.TextField(null=True)
-
+  
+  def set_token(self, token: str):
+    self.auth_token = token
+    return self.save()
