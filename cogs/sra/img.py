7dd1405bb49b9193da79d2afbeea05dbabcf906e
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2025-06-05 22:04:42 +0530

    Update img.py

--- cogs/sra/img.py
+++ cogs/sra/img.py
@@ -12,10 +12,29 @@ class export:
         "Available types:\n```\n"+"\n".join(opts)+"\n```"
       )
 
-    return await ctx.reply(
-      (await self.fetch("img/"+of))["link"]
+    return await self.send_image_embed(ctx, (await self.fetch("img/"+of))["link"]
     )
+  @commands.command(aliases=["anime"])
+  async def animu(self, ctx, content: str = ""):
+    opts=['nom', 'poke', 'cry', 'kiss', 'pat', 'hug', 'wink', 'facepalm', 'quote']
+    of=content.lower().replace("facepalm", "face-palm")
+    if (not of) or of not in opts:
+      return await ctx.reply(
+        "Available types:\n```\n"+"\n".join(opts)+"\n```"
+      )
+
+    res=await self.fetch("animu/"+of)
+    if of!="quote":
+      return await self.send_image_embed(ctx, res["link"])
+      
+    e=discord.Embed(
+      title=res["anime"],
+      description=res["name"]+": "+res["quote"],
+      color=0x000
+    )
+    return await ctx.reply(embed=e)
     
+  
   @commands.command(name="filter")
   async def filter_cmd(self, ctx, filter: str = "", user: discord.User = None):
     opts="blue blurple pixelate blur blurple2 green greyscale invert red invertgreyscale sepia".split(" ")
@@ -34,14 +53,41 @@ class export:
           im=m.url
           break
       if not im:
-        return await ctx.reply("Only pmg format is supported!")
+        return await ctx.reply("Only png format is supported!")
     elif ctx.author.avatar and ctx.author.avatar.url:
       im=ctx.author.avatar.url
       
     if not im:
       return await ctx.reply('Couldnt find an image!')
       
-    return await ctx.reply("https://api.some-random-api.com/canvas/filter/"+filter+"?"+self.urlfmt(dict(avatar=im)))
+    return await self.send_image_embed(ctx, "https://api.some-random-api.com/canvas/filter/"+filter+"?"+self.urlfmt(dict(avatar=im)))
+    
+  @commands.command(name="overlay")
+  async def overlay_cmd(self, ctx, overlay: str = "", user: discord.User = None):
+    opts="comrade glass jail passed triggered wasted".split(" ")
+    overlay=overlay.lower()
+    if (not overlay) or overlay not in opts:
+      return await ctx.reply(
+        "Available types:\n```\n"+"\n".join(opts)+"\n```"
+      )
+    im=""
+    if user and user.avatar:
+      im=user.avatar.url
+    if im:...
+    elif ctx.message.attachments:
+      for m in ctx.message.attachments:
+        if m.content_type=="image/png":
+          im=m.url
+          break
+      if not im:
+        return await ctx.reply("Only png format is supported!")
+    elif ctx.author.avatar and ctx.author.avatar.url:
+      im=ctx.author.avatar.url
+      
+    if not im:
+      return await ctx.reply('Couldnt find an image!')
+      
+    return await self.send_image_embed(ctx, "https://api.some-random-api.com/canvas/overlay/"+overlay+"?"+self.urlfmt(dict(avatar=im)))
     
   @commands.command(name="mask")
   async def mask_cmd(self, ctx, mask: str = "", user: discord.User = None):
@@ -61,19 +107,19 @@ class export:
           im=m.url
           break
       if not im:
-        return await ctx.reply("Only pmg format is supported!")
+        return await ctx.reply("Only png format is supported!")
     elif ctx.author.avatar and ctx.author.avatar.url:
       im=ctx.author.avatar.url
       
     if not im:
       return await ctx.reply('Couldnt find an image!')
       
-    return await ctx.reply("https://api.some-random-api.com/canvas/misc/"+mask+"?"+self.urlfmt(dict(avatar=im)))
+    return await self.send_image_embed(ctx, "https://api.some-random-api.com/canvas/misc/"+mask+"?"+self.urlfmt(dict(avatar=im)))
     
   @commands.command()
   async def tweet(self, ctx, *tweet: str):
     tweet=" ".join(tweet)
-    return await ctx.reply("https://api.some-random-api.com/canvas/misc/tweet?"+self.urlfmt(dict(
+    return await self.send_image_embed(ctx, "https://api.some-random-api.com/canvas/misc/tweet?"+self.urlfmt(dict(
       displayname=ctx.author.display_name,
       username=ctx.author.name,
       comment=tweet,
@@ -84,7 +130,7 @@ class export:
   @commands.command()
   async def comment(self, ctx, *comment: str):
     comment=" ".join(comment)
-    return await ctx.reply("https://api.some-random-api.com/canvas/misc/youtube-comment?"+self.urlfmt(dict(
+    return await self.send_image_embed(ctx, "https://api.some-random-api.com/canvas/misc/youtube-comment?"+self.urlfmt(dict(
       username=ctx.author.name,
       comment=comment,
       avatar=ctx.author.avatar.url,
@@ -96,4 +142,5 @@ mask_cmd=export.mask_cmd
 filter_cmd = export.filter_cmd
 tweet_cmd = export.tweet
 comment_cmd = export.comment
-# exports = {"img": img}
+overlay_cmd = export.overlay_cmd
+anime_cmd = export.animu
