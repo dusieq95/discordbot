63d7c8367eb99e85244224129092db3151b5399f
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2025-06-05 16:47:26 +0530

    Update __init__.py

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,10 +1,12 @@
-exts = ["ai", "func"]
+exts = ["ai", "sra"]
 async def setup(bot: commands.Bot):
   """Function to load the cog"""
   for ext in exts:
+    ext="cogs."+ext
     await bot.load_extension(ext)
 
 async def teardown(bot: commands.Bot):
   """Function to unload the cog"""
   for ext in exts:
+    ext="cogs."+ext
     await bot.unload_extension(ext)
