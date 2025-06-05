bae197d09997b69225ae830e0327c4c125087bfa
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2025-06-05 17:03:11 +0530

    Create img.py

---
+++ cogs/sra/img.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+class export:
+  @commands.command()
+  async def img(self, ctx, of: str = ""):
+    opts=['fox', 'cat', 'bird', 'panda', 'red_panda', 'pikachu', 'racoon', 'koala', 'kangaroo', 'whale', 'dog', 'bird']
+    of=of.lower()
+    if (not of) or of not in opts:
+      return await ctx.send(
+        "Available types:\n```\n"+"\n".join(opts)+"\n```"
+      )
+
+    return await ctx.send(
+      await self.fetch(of)
+    )
+
+
+exports = [export()]
